f2579535ee7dc39180be9630c4a220720c1ebb1e
jmendoza1705 <julianamendoza8@gmail.com>
2023-05-03 14:13:05 -0500

    Metricas MP1

---
+++ Proyecto 2.py
@@ -0,0 +1,183 @@
+# Alejandra Erazo / Juliana Mendoza
+# Modelo
+
+## Médicos que quieran incorporar un apoyo de analítica de datos en el proceso de evaluación de pacientes y
+# la toma de decisiones asociada (solicitud de exámenes, chequeos y otros procedimientos).
+
+# Se importan las librerias
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from pgmpy.models import BayesianNetwork
+from pgmpy.inference import VariableElimination
+from pgmpy.estimators import MaximumLikelihoodEstimator
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+##
+# Se leen los datos
+data =  pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.cleveland.data', header=None)
+names= ["age","sex", "cp", "trestbps", "chol", "fbs", "restecg", "thalach", "exang", "oldpeak", "slope","ca", "thal", "num"]
+data.columns = names
+data['ca'] = pd.to_numeric(data['ca'], errors='coerce')
+data['thal'] = pd.to_numeric(data['thal'], errors='coerce')
+data = data.astype(float)
+##
+# Se eliminan los Nan y se convierte a un arreglo de Numpy
+data = data.dropna()
+data = data.to_numpy()
+
+# Se estandarizan las variables para el diagnostico:
+# 0 -- No presenta heart disease
+# 1 -- mild heart disease
+# 3 -- severe heart disease
+for j in range(0, data.shape[0]):
+    if data[j, 13] == 2:
+        data[j, 13] = 1
+    elif data[j, 13] == 4:
+        data[j, 13] = 3
+
+
+# Discretizacion del colesterol
+# menos de 200 -- Deseable
+# de 200 a 239 -- En el limite superior
+# mas de 240 -- alto
+# https://www.mayoclinic.org/es-es/tests-procedures/cholesterol-test/about/pac-20384601
+
+for j in range(0, data.shape[0]):
+    if data[j, 4] < 200:
+        data[j, 4] = 0
+    elif (200 <= data[j, 4] < 240):
+        data[j, 4] = 1
+    elif data[j, 4] >= 240:
+        data[j, 4] = 2
+
+# Discretización de OldPeak
+# Menos de 2 - 0
+# Entre 2 y 4 - 1
+# Mayor o igual a 4 - 2
+
+for j in range(0, data.shape[0]):
+    if data[j, 9] < 2:
+        data[j, 9] = 0
+    elif (2 <= data[j, 9] < 4):
+        data[j, 9] = 1
+    elif data[j, 9] >= 4:
+        data[j, 9] = 2
+
+
+# Discretización de la edad
+# 29 a 39 -- 30
+# 40 a 49 -- 40
+# 50 a 59 -- 50
+# 60 a 69 -- 60
+# Mayor o igual a 70 -- 70
+
+for j in range(0, data.shape[0]):
+    if (29 <= data[j, 0] < 40):
+        data[j, 0] = 30
+    elif (40 <= data[j, 0] < 50):
+        data[j, 0] = 40
+    elif (50 <= data[j, 0] < 60):
+        data[j, 0] = 50
+    elif (60 <= data[j, 0] < 70):
+        data[j, 0] = 60
+    elif data[j, 0] >= 70 :
+        data[j, 0] = 70
+##
+
+#
+#Se dividen los datos
+
+DataEntrenamiento = data[0:250,]
+DataValidacion = data[250:,]
+
+#Se define el modelo
+##
+# Se define la red bayesiana
+modelo_HD = BayesianNetwork([("AGE", "CHOL"), ("FBS", "CHOL"), ("CHOL", "HD"), ("THAL", "HD"), ("HD", "EXANG"),
+                         ("HD", "OLDPEAK")])
+
+# Se definen las muestras
+info = np.zeros((250,7))
+columnas = [0, 4, 5, 8, 9, 12, 13]
+nombres = ["AGE", "CHOL", "FBS", "EXANG", "OLDPEAK", "THAL", "HD"]
+for i in range(len(columnas)):
+    info[:,i] = DataEntrenamiento[:,columnas[i]]
+muestras = pd.DataFrame(info, columns = nombres)
+
+#Estimación de las CPDs
+modelo_HD.fit(data = muestras, estimator = MaximumLikelihoodEstimator)
+for i in modelo_HD.nodes():
+    print("CPD ", i,"\n", modelo_HD.get_cpds(i))
+
+#Se imprime completa la CDP HD
+for i in range(len(modelo_HD.get_cpds("HD").values)):
+    print(modelo_HD.get_cpds("HD").values[i])
+
+# Se realiza la eliminación de variables
+infer = VariableElimination(modelo_HD)
+##
+# Se definen valores para los parámetros para la predicción
+age = 60
+Fbs = 1
+Chol = 2
+st = 1
+ex = 1
+tal = 7
+
+# Se realiza la predicción teniendo en cuenta la evidencia
+posterior_p = infer.query(["HD"], evidence={"AGE": age, "FBS": Fbs, "CHOL": Chol,  "OLDPEAK": st, "EXANG": ex, "THAL": tal})
+print(posterior_p)
+valores = posterior_p.values
+print(valores)
+
+#######
+
+Val = np.zeros((47,7))
+columnas = [0, 4, 5, 8, 9, 12, 13]
+nombres = ["AGE", "CHOL", "FBS", "EXANG", "OLDPEAK", "THAL", "HD"]
+for i in range(len(columnas)):
+    Val[:,i] = DataValidacion[:,columnas[i]]
+
+##
+predicciones = []
+anotaciones = []
+
+for i in range(0, len(Val)):
+    age, chol, Fbs, ex, st, tal = Val[i, 0], Val[i, 1], Val[i, 2], Val[i, 3], \
+                                  Val[i, 4], Val[i, 5]
+
+    anotaciones = np.append(anotaciones, Val[i, 6])
+
+    posterior_p = infer.query(["HD"],evidence={"AGE": age, "FBS": Fbs, "CHOL": Chol,
+                                               "OLDPEAK": st, "EXANG": ex, "THAL": tal})
+
+    probabilidades = posterior_p.values
+
+    if np.isnan(probabilidades).any():
+        anotaciones = np.delete(anotaciones, i, axis=0)
+
+    maximo = np.max(probabilidades)
+    for j in range(0, len(probabilidades)):
+        if probabilidades[j] == maximo:
+            posicion = j
+            if posicion == 2:
+                predicciones = np.append(predicciones, 3)
+            else:
+                predicciones = np.append(predicciones, posicion)
+##
+matriz = confusion_matrix(anotaciones, predicciones)
+
+cm_display = ConfusionMatrixDisplay(confusion_matrix = matriz,
+                                    display_labels = ['No EC', 'EC Leve','EC Severa'])
+
+
+cm_display.plot(cmap = 'PuRd', colorbar = True)
+plt.title('Matriz de Confusión para Predicción de Enfermedad Cardiaca (EC) \n')
+plt.ylabel('Anotaciones')
+plt.xlabel('Predicciones')
+plt.tight_layout()
+plt.show()
+
+
+
